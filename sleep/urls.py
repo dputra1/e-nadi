9c9bac66dcf686a20146dc8c420d3943bf3534f7
mahdi imani wafi <73066863+mahdiwafi@users.noreply.github.com>
2021-12-29 15:12:18 +0700

    changes

--- sleep/urls.py
+++ sleep/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import mainpage_sleep, new_sleep, update_sleep, reset_sleep
+from .views import mainpage_sleep, new_sleep, update_sleep, reset_sleep, get_sleep
 
 urlpatterns = [
     path('', mainpage_sleep, name='mainpage_s'),
     path('new', new_sleep, name='new_s'),
     path('update', update_sleep, name='update_s'),
-    path('reset', reset_sleep, name='reset_s')
+    path('reset', reset_sleep, name='reset_s'),
+    path('get_sleep', get_sleep, name='get-sleep'),
 ]
